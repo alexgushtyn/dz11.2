e3bab7106b54b5ad4b2575332d50e1869ce13f8f
Alex Gushtyn <sashabaltnk@gmail.com>
2023-10-12 19:13:20 +0300

    add second func and docsting

--- str_func.py
+++ str_func.py
@@ -1,4 +1,11 @@
 def upper():
     s1 = input("Введите текст")
+    """Всё заглавными буквами"""
     s2 = s1.upper()
+    return s2
+
+def first_upper():
+    s1 = input("Введите текст")
+    """Первые буквы в заглавные"""
+    s2 = s1.title()
     return s2
